76ff0563881bec2b2f051f35c9fbde8758cb888d
enakerueos <danzannatyrov@yandex.ru>
2025-03-07 13:00:17 +0300

    Update Green_5.cs

--- Green_5.cs
+++ Green_5.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using static Lab_6.Green_5;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Lab_6
 {
@@ -14,12 +17,39 @@ namespace Lab_6
 
             public string Name => name1;
             public string Surname => surname1;
-            public int[] Marks => marks1.ToArray();
-            public double AvgMark => marks1.Average();
+            public int[] Marks
+            {
+                get
+                {
+                    if (marks1 == null)
+                    {
+                        return null;
+                    }
+                    int[] arrays = new int[marks1.Length];
+                    Array.Copy(marks1, arrays, marks1.Length);
+                    return arrays;
+                }
+            }
+            public double AvgMark
+            {
+                get
+                {
+                    if (marks1 == null || marks1.Length == 0)
+                    {
+                        return 0;
+                    }
+                    double summa = 0;
+                    for (int i = 0; i < marks1.Length; i++)
+                    {
+                        summa += marks1[i];
+                    }
+                    return summa / marks1.Length;
+                }
+            }
 
             public Student(string name, string surname)
             {
-                name1 = name;
+                name1  = name;
                 surname1 = surname;
                 marks1 = new int[5];
             }
@@ -28,89 +58,134 @@ namespace Lab_6
             {
                 if (mark < 2 || mark > 5)
                 {
-                    Console.WriteLine("не те оценки");
                     return;
                 }
+                if (marks1 == null)
+                {
+                    return;
+                }
+
+
                 for (int i = 0; i < marks1.Length; i++)
                 {
                     if (marks1[i] == 0)
                     {
                         marks1[i] = mark;
-                        return;
+                        break;
                     }
                 }
 
-                Console.WriteLine("написали всё");
+                Console.WriteLine("экзамены написаны");
             }
 
             public void Print()
             {
-                Console.WriteLine($"{Name} {Surname}, ср арифм балл: {AvgMark:F2}");
+                Console.WriteLine($"{Name} {Surname}");
+                Console.WriteLine($"балл ср арифм: {AvgMark:F2}");
             }
         }
 
         public struct Group
         {
             private string name1;
-            private Student[] students;
+            private Student[] students1;
+            private int cnt1;
 
             public string Name => name1;
-            public Student[] Students => students.ToArray();
-            public double AvgMark => students.Average(s => s.AvgMark);
+            public Student[] Students => students1;
+
+            public double AvgMark
+            {
+                get
+                {
+                    if (students1 == null || cnt1 == 0)
+                    {
+                        return 0;
+                    }
+
+                    int t = 0;
+                    double sr = 0;
+
+                    for (int i = 0; i < cnt1; i++)
+                    {
+                        if (students1[i].AvgMark > 0)
+                        {
+                            sr += students1[i].AvgMark;
+                            t++;
+                        }
+                    }
+                    return t == 0 ? 0 : sr / t;
+                }
+            }
 
             public Group(string name)
             {
                 name1 = name;
-                students = new Student[0];
+                students1 = new Student[0];
+                cnt1 = 0;
             }
 
             public void Add(Student student)
             {
-                Array.Resize(ref students, students.Length + 1);
-                students[students.Length - 1] = student;
+                if (students1 == null)
+                    students1 = new Student[0];
+                Array.Resize(ref students1, cnt1 + 1);
+                students1[cnt1] = student;
+                cnt1++;
             }
 
-            public void Add(Student[] newStudents)
+            public void Add(Student[] Students)
             {
-                if (newStudents == null || newStudents.Length == 0)
+                if (Students == null)
                 {
                     return;
                 }
-                var studentList = students.ToList();
-                studentList.AddRange(newStudents); // добавляем массив студентов
-                students = studentList.ToArray();
-            }
-
-            public void Print()
-            {
-                Console.WriteLine($"группа: {Name} , балл : {AvgMark:F2}");
-
-                foreach (var student in students)
+                if (students1 == null)
                 {
-                    student.Print();
+                    students1 = new Student[0];
                 }
+
+                int new_length = cnt1 + Students.Length;
+
+                Array.Resize(ref students1, new_length);
+                for (int i = 0; i < Students.Length; i++)
+                {
+                    students1[cnt1 + i] = Students[i];
+                }
+                cnt1 = new_length;
             }
 
             public static void SortByAvgMark(Group[] groups)
             {
                 if (groups == null || groups.Length == 0)
-                {
                     return;
-                }
-
-                for (int i = 0; i < groups.Length - 1; i++)
+                bool n;
+                do
                 {
-                    for (int j = 0; j < groups.Length - 1 - i; j++)
+                    n = false;
+                    for (int i = 0; i < groups.Length - 1; i++)
                     {
-                        if (groups[j].AvgMark < groups[j + 1].AvgMark)
+                        if (groups[i].AvgMark < groups[i + 1].AvgMark)
                         {
-                            Group temp = groups[j];
-                            groups[j] = groups[j + 1];
-                            groups[j + 1] = temp;
+                            Group t = groups[i];
+                            groups[i] = groups[i + 1];
+                            groups[i + 1] = t;
+                            n = true;
                         }
                     }
+                } while (n);
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{Name} :Балл: {AvgMark:F2}");
+
+                foreach (var student in students1)
+                {
+                    student.Print();
                 }
             }
+            
         }
     }
 }
